45c15ee0e413e5200e1e1e659a711add0efc9bb2
Mayank <mayankrouniyar9300@gmail.com>
2025-09-17 11:15:18 +0530

    Pushing Login and Signup

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,13 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 const app=express()
+const allowedOrigin=[
+    "http://localhost:5173",
+]
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+        origin:allowedOrigin,
+        credentials:true,
+    }))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended : true, limit:"16kb"}))
 app.use(express.static("public"))

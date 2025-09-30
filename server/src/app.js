1c024649b55b1564e19b40402c92c6726a85ce90
Dev Saxena <devsaxena7668@gmail.com>
2025-09-30 12:57:41 +0530

    code updated

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ import archivesRouter from "./routes/archives.route.js";
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173" , "https://monastery360-70f5.onrender.com"], 
+  origin: "http://localhost:5173" || "https://monastery360-70f5.onrender.com", 
   credentials: true
 }));
 app.use(express.json({ limit: "16kb" }));

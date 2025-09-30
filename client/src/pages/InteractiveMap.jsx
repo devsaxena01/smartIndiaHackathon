ecf147acc548b26eca89ca8c619277a238c1fd2f
Dev Saxena <devsaxena7668@gmail.com>
2025-09-30 22:44:02 +0530

    code updated

--- client/src/pages/InteractiveMap.jsx
+++ client/src/pages/InteractiveMap.jsx
@@ -11,7 +11,7 @@ const InteractiveMap = () => {
   useEffect(() => {
     const fetchMonastery = async () => {
       try {
-        const res = await fetch(`http://localhost:5000/api/v1/monasteries/${id}`);
+        const res = await fetch(`https://monastery.onrender.com/api/v1/monasteries/${id}`);
         const data = await res.json();
         setMonastery(data.data || null);
       } catch (err) {
